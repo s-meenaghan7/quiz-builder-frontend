6c7358fc782060ca3ee161d536e85a0fdd164781
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-09-29 18:53:18 -0700

    deleted unnecessary files. Project is ready to start development!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+
+// todo: learn more about forms
+// todo: build the entire form here and split parts of the form into components once complete.
+// you know how you want it to look, now need to implement it.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello world</h1>
     </div>
   );
 }
