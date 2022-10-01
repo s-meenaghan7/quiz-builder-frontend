2dec56a103282449b7d424c035ac12b6e92f67d0
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-09-30 17:12:07 -0700

    General static mockup continues to be built in single file. Will split into separate components when ready. Table being used for each answer in the answer section.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
 import './styles/App.css';
+import './styles/AnswerSection.css';
+import './styles/QuestionSection.css';
+import './styles/Answer.css';
+import './styles/Controls.css';
 
 // todo: learn more about forms
-// todo: build the entire form here and split parts of the form into components once complete.
+// todo: build the entire form here and split parts of the form into components once complete. static version first!
 // you know how you want it to look, now need to implement it.
+
+// use a table to organize the answer section?
+    // column 1 (Starting from left) > Number
+    // column 2 > Textfield for the answer
+    // column 3 > radio button for if that answer is correct for this q
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello world</h1>
+      <form>
+        <div className='question-section'>
+          <label>Question
+            <input />
+          </label>
+        </div>
+
+        <div className='answer-section'>
+
+          <label className='answer'>1
+            <input />
+            <input type='radio' name='isCorrect' />
+          </label>
+
+          <label className='answer'>2
+            <input />
+            <input type='radio' name='isCorrect' />
+          </label>
+
+        </div>
+
+        {/* table sample, probably won't need the header */}
+        <table width='100%'>
+          <tr>
+            <th>Number</th>
+            <th>Answer</th>
+            <th>Is Correct?</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td><input type='text' /></td>
+            <td><input type='radio' name='isCorrect' /></td>
+          </tr>
+
+          <tr>
+            <td>2</td>
+            <td><input type='text' /></td>
+            <td><input type='radio' name='isCorrect' /></td>
+          </tr>
+        </table>
+
+        <div className='controls'>
+          <button type='button'>-</button>
+          <button type='button'>+</button>
+        </div>
+
+      </form>
     </div>
   );
 }
