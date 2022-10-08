9356b96c268396ad24787abc77e57d14bdec0661
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-10-08 15:30:18 -0700

    Refactored Answer component, now includes useEffect hook to emulate the componentDidMount lifecycle method. Passed in 2 functions from parent component, setAnswerData and setCorrectAnswer, both of which are used to take data from the Answer component and give it to the AnswerSection parent component, which is used to update the answers state array. This keeps the answers data up to date across the application.

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Answer.css';
 
-export default function Answer({ id }) {
+export default function Answer({ id, setAnswerData, setCorrectAnswer }) {
+
+    const [answer, setAnswer] = useState({
+                id: id,
+                answer: "",
+    });
+
+    useEffect(() => {
+        setAnswerData(answer);
+    }, []);
+
+    const changeAnswerHandler = (event) => {
+        answer.answer = event.target.value;
+        setAnswerData(answer);
+        setCorrectAnswer();
+    }
+
+    const radioButtonHandler = (event) => {
+        setCorrectAnswer();
+    }
+
     return (
-        <tr>
+        <tr className='answer'>
             <td> {id} </td>
-            <td> <input type='text' /> </td>
-            <td> <input type='radio' name='isCorrect' /> </td>
+            <td> <input type='text' name='answerField' onChange={ (e) => changeAnswerHandler(e) } /> </td>
+            <td> <input type='radio' name='isCorrect' onChange={ (e) => radioButtonHandler(e) } /> </td>
         </tr>
     );
 }
