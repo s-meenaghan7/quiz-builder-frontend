0d4f8a480f0dd3202eda8e1d732cf412ca3418a9
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-10-08 15:32:42 -0700

    Created AnswerSection component, refactored code from main App.js file to instead use this component to render the table of answers. Defines 2 functions to be passed to Answer child components to retrieve data and set that data to the array of answers.

--- src/App.js
+++ src/App.js
@@ -3,62 +3,60 @@ import './styles/AnswerSection.css';
 import './styles/QuestionSection.css';
 import './styles/QuestionControls.css';
 import { useState } from 'react';
-import Answer from './components/Answer';
 import AnswerControls from './components/AnswerControls';
+import AnswerSection from './components/AnswerSection';
 
 function App() {
 
   let [numberOfAnswers, setNumberOfAnswers] = useState(2);
-  const [answers, setAnswers] = useState(Array(numberOfAnswers).fill( <Answer /> ));
-  const [quizData, setQuizData] = useState([1]);
+  const [answers, setAnswers] = useState(Array(numberOfAnswers).fill( {} ));
+  const [quizData, setQuizData] = useState(Array(1));
+
+  const createNewQuestion = () => {
+    // if (document.querySelector('#questionField').value === "") console.log('empty');
+
+    // create the question object
+    const question = {
+      question: document.querySelector('#questionField').value,
+      options: [
+        // TODO: Answer.jsx; figuring out how to create an answer object, and those objects will be held in the answers array (state)
+      ]
+    }
+
+
+  }
 
   return (
     <div className='App'>
-
-      <div className='question-controls'>
-        <button type='button'>Previous Question</button>
-        <button type='button'>New Question</button>
-        <button type='button'>Submit Quiz</button>
-      </div>
-
       <form>
+
+        <div className='question-controls'>
+          <button type='button'>Previous Question</button>
+          <button type='button' onClick={ () => createNewQuestion() }>New Question</button>
+          <button type='button'>Submit Quiz</button>
+        </div>
+
+        <div className='question-test-controls'>
+          <button type='button' onClick={ () => console.log(quizData) }>Get Quiz Data</button>
+        </div>
 
         <div className='question-section'>
           <h3>Question: {quizData.length}</h3>
 
           <label>
-            <input type='text'/>
+            <input name='questionField' id='questionField' type='text' required/>
           </label>
         </div>
 
         <AnswerControls 
           answers={answers}
-          numberOfAnswers={numberOfAnswers}
+          numberOfAnswers={answers.length}
           setNumberOfAnswers={setNumberOfAnswers}
         />
 
-        <div className='test-controls'>
-          <button type='button' onClick={ () => console.log(numberOfAnswers) }>Number of Answers</button>
-          <button type='button' onClick={ () => console.log(answers) }>Answers List</button>
-        </div>
-
-        <table className='answer-section' width='100%'>
-          <thead>
-            <tr>
-              <th>Answer ID</th>
-              <th>Answer</th>
-              <th>Is Correct?</th>
-            </tr>
-          </thead>
-          
-          <tbody>
-            {
-              answers.map((a, i) => 
-                <Answer key={i + 1} id={i + 1} />
-              )
-            }
-          </tbody>
-        </table>
+        <AnswerSection 
+          answers={answers}
+        />
 
       </form>
     </div>
---
+++ src/components/AnswerSection.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Answer from './Answer';
+
+export default function AnswerSection({ answers }) {
+    
+    const setAnswerData = (answer) => {
+        const index = answer.id - 1;
+        answers[index] = {...answer};
+    }
+
+    const setCorrectAnswer = () => {
+        const radios = document.querySelectorAll('input[name="isCorrect"]');
+
+        for (let i = 0; i < radios.length; ++i) {
+            answers[i].isCorrect = radios[i].checked;
+        }
+    }
+
+    return (
+        <div className='answer-section'>
+            <table id='answer-table' width='100%'>
+                <thead>
+                    <tr>
+                        <th>Answer ID</th>
+                        <th>Answer</th>
+                        <th>Is Correct?</th>
+                    </tr>
+                </thead>
+            
+                <tbody>
+                    {
+                        answers.map((a, i) => 
+                            <Answer 
+                                key={i + 1} id={i + 1}
+                                setAnswerData={setAnswerData}
+                                setCorrectAnswer={setCorrectAnswer}
+                            />
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    );
+}
