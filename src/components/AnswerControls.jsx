61b96f15f886cf5185d005e5e40e370a1550f780
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-10-08 15:26:15 -0700

    Controls no longer adds an Answer component to the answers array, now pushes an object to have data populated to it from other components. Removed Answer.jsx import, and added the test controls to this component, to be hidden later when no longer needed.

--- src/components/AnswerControls.jsx
+++ src/components/AnswerControls.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import '../styles/AnswerControls.css';
-import Answer from './Answer';
 
 export default function AnswerControls({answers, numberOfAnswers, setNumberOfAnswers}) {
 
     const addAnswer = () => {
         setNumberOfAnswers(numberOfAnswers + 1);
-        answers.push( <Answer /> );
+        answers.push( {} );
     }
 
     const subtractAnswer = () => {
@@ -17,9 +16,16 @@ export default function AnswerControls({answers, numberOfAnswers, setNumberOfAns
     }
 
     return (
-        <div className='answer-controls'>
-          <button type='button' onClick={ () => subtractAnswer() }>-</button>
-          <button type='button' onClick={ () => addAnswer() }>+</button>
-        </div>
+        <>
+            <div className='answer-controls'>
+                <button type='button' onClick={ () => subtractAnswer() }>-</button>
+                <button type='button' onClick={ () => addAnswer() }>+</button>
+            </div>
+
+            <div className='test-controls'>
+                <button type='button' onClick={ () => console.log(answers.length) }>Number of Answers</button>
+                <button type='button' onClick={ () => console.log(answers) }>Answers Data</button>
+            </div>
+        </>
     );
 }
