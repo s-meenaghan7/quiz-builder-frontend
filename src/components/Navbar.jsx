0ac7f02bb04bbb75a99459a4c9665ee984fc0143
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-10-27 18:45:28 -0700

    Created main webpage Navbar component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+export default function Navbar(props) {
+    return (
+        <div className='navbar'>
+            
+        </div>
+    );
+}
