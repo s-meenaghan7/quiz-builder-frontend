f3daedf91e240e1ba7567eae6768f019b70a493b
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-12-20 16:52:18 -0800

    created files and package structure for the LoginForm.

---
+++ src/app/loginform/LoginForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoginForm.css';
+
+function LoginForm(props) {
+  return (
+    <div>
+      Hello, Login Form!
+    </div>
+  );
+}
+
+export default LoginForm;
