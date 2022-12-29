86b05a72504edac3ecdde0fbbca4b310ef2b9834
Sean Meenaghan <seanmeenaghan@gmail.com>
2022-12-28 18:03:28 -0800

    Created a basic RegisterForm component, JSX and CSS only thus far

---
+++ src/app/registerform/RegisterForm.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './RegisterForm.css';
+
+export default function RegisterForm(props) {
+
+  return (
+    <div className='registerform'>
+      <form autoComplete='off'>
+        <h1 className='registerform_title'>Create Account!</h1>
+
+        <label htmlFor='name'>Your Name</label>
+        <input
+          className='registerform_input'
+          type="text"
+          placeholder="Full Name"
+          name="name"
+          required
+        />
+
+        <label htmlFor='email'>Email</label>
+        <input
+          className='registerform_input'
+          type="text"
+          placeholder="Email"
+          name="email"
+          required
+        />
+
+        <label htmlFor='password'>Password</label>
+        <input
+          className='registerform_input'
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+        />
+
+        <label htmlFor='confirm_password'>Confirm Password</label>
+        <input
+          className='registerform_input'
+          type="password"
+          placeholder="Confirm Password"
+          name="confirm_password"
+          required
+        />
+
+        <button
+          type='button'
+          className='register_btn'
+        >
+          CREATE ACCOUNT
+        </button>
+
+      </form>
+    </div>
+  );
+}
