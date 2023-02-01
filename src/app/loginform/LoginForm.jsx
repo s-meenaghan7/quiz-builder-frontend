f3fd01a09a4049bb418b8bd31fbed03173ab9aca
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-02-01 13:00:23 -0800

    Added a login error field, to notify the user if they enter invalid credentials. Minor JSX updates, too.

--- src/app/loginform/LoginForm.jsx
+++ src/app/loginform/LoginForm.jsx
@@ -5,8 +5,9 @@ export default function LoginForm(props) {
 
   return (
     <div className='loginform'>
-      <form autoComplete='off'>
+      <form>
         <h1 className='login_title'>Log in</h1>
+        <p id='login_error'></p>
 
         <label htmlFor='email'>Email</label>
         <input
@@ -34,9 +35,19 @@ export default function LoginForm(props) {
         </button>
 
         <div className='links_container'>
-          <a href='localhost:3000/#'>New? Create a free account!</a>
+          <a
+            href='localhost:3000/#'
+            title='Create a free account!'
+          >
+            New? Create a free account!
+          </a>
           <p id='or'>OR</p>
-          <a href='localhost:3000/#'>Continue as Guest!</a>
+          <a
+            href='localhost:3000/#'
+            title='Continue as Guest to try out Quiz Builder free, no sign up required!'
+          >
+            Continue as Guest!
+          </a>
         </div>
  
       </form>
