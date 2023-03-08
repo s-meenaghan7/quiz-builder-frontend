44a8f3497fbd6ef44ec882478100fedc0499d094
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-03-07 17:39:29 -0800

    Updated RegistrationService

--- src/service/RegistrationService.js
+++ src/service/RegistrationService.js
@@ -5,7 +5,14 @@ const REGISTRATION_API_BASE_URL = 'http://localhost:8080/api/v1/register';
 class RegistrationService {
 
   registerUser(registrationRequest) {
-    return axios.post(REGISTRATION_API_BASE_URL, registrationRequest);
+    return axios.post(
+      REGISTRATION_API_BASE_URL, 
+      registrationRequest,
+      {
+        headers: { 'Content-Type' : 'application/json' },
+        withCredentials: true
+      }
+    );
   }
 
 }
