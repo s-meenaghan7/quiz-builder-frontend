8ef19146494f1ba2ec43659ef0a2238a606bab38
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-03-13 21:06:25 -0700

    Implemented ClipLoader for loading animation, email material icon, and styling for both

--- src/app/registerform/components/VerificationPage.jsx
+++ src/app/registerform/components/VerificationPage.jsx
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 import '../styles/VerificationPage.css';
 import { ClipLoader } from 'react-spinners';
+import RegistrationService from '../../../service/RegistrationService';
 
 export default function VerificationPage({ email }) {
   const [loading, setLoading] = useState(false);
 
-  const resendEmail = () => {
-    // TODO
-    console.log('resending email...');
+  const resendEmail = async () => {
+    setLoading(true);
+
+    try {
+      // await RegistrationService.resendEmail(email);
+      await RegistrationService.resendEmail(email);
+      // ToastService.success("Email to {email} resent successfully.");
+    } catch (err) {
+      if (!err?.response) {
+        // ToastService.warn('Error: Server unavailable, please try again later.');
+      } else {
+        // ToastService.warn('Error: Something went wrong, please try again later.');
+      }
+    }
+
+    setLoading(false);
   }
 
   return (
@@ -18,7 +32,9 @@ export default function VerificationPage({ email }) {
         Your registration was successful. You will receive an email from QuizMe with a link to activate your account. Please check your email: {email ? email : 'null'}.
       </p>
 
-      {/* insert img of envelope */}
+      <span id='mail_icon' className="material-icons-outlined">
+        mark_as_unread
+      </span>
 
       <p>
         Your account must be activated before you can login to QuizMe.
@@ -28,14 +44,16 @@ export default function VerificationPage({ email }) {
 
       {
         loading ?
-          <ClipLoader
-            color={'#ffc700'}
-            loading={loading}
-            cssOverride={{}}
-            size={50}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
+          <div className='spinner_container'>
+            <ClipLoader
+              color={'#ffc700'}
+              loading={loading}
+              cssOverride={{}}
+              size={50}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+          </div>
           :
           <button
             title='Click here to resend your verification email'
