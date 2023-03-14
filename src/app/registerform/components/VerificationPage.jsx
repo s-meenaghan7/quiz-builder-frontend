00d4d55cf56d2c24a21aa88193f2fde67c83afdc
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-03-14 14:18:49 -0700

    Implemented resendEmail function in VerificationPage component.

--- src/app/registerform/components/VerificationPage.jsx
+++ src/app/registerform/components/VerificationPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import '../styles/VerificationPage.css';
 import { ClipLoader } from 'react-spinners';
 import RegistrationService from '../../../service/RegistrationService';
+import ToastService from '../../toasts/ToastService';
 
 export default function VerificationPage({ email }) {
   const [loading, setLoading] = useState(false);
@@ -10,14 +11,14 @@ export default function VerificationPage({ email }) {
     setLoading(true);
 
     try {
-      // await RegistrationService.resendEmail(email);
       await RegistrationService.resendEmail(email);
-      // ToastService.success("Email to {email} resent successfully.");
+      ToastService.success(`Email to ${email} resent successfully.`);
+
     } catch (err) {
       if (!err?.response) {
-        // ToastService.warn('Error: Server unavailable, please try again later.');
+        ToastService.warn('Error: Server unavailable, please try again later.');
       } else {
-        // ToastService.warn('Error: Something went wrong, please try again later.');
+        ToastService.warn('Error: Something went wrong, please try again later.');
       }
     }
 
@@ -29,7 +30,7 @@ export default function VerificationPage({ email }) {
       <h1>Email Verification</h1>
 
       <p>
-        Your registration was successful. You will receive an email from QuizMe with a link to activate your account. Please check your email: {email ? email : 'null'}.
+        Your registration was successful! You will receive an email from QuizMe with a link to activate your account. Please check your email: {email ? email : 'null'}.
       </p>
 
       <span id='mail_icon' className="material-icons-outlined">
