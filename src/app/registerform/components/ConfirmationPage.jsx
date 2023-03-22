4e2f32b0e75f4b59fc35c00b9e7909b15228975f
Sean Meenaghan <seanmeenaghan@gmail.com>
2023-03-21 22:27:03 -0700

    Implemented ConfirmationPage and styling, to be refactored. Currently, a lot of code that needs to be DRY'd, particularly the JSX. I should be able to use state and change it depending on isSuccess and/or isError, and then apply that to the JSX, without have to have 2 return statements.

--- src/app/registerform/components/ConfirmationPage.jsx
+++ src/app/registerform/components/ConfirmationPage.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useConfirmToken } from '../../../hooks/useConfirmToken.js';
 import '../styles/ConfirmationPage.css';
 
-export default function ConfirmationPage(props) {
+export default function ConfirmationPage() {
+  const { token } = useParams();
+  const { mutate: confirmToken, isSuccess, isError, error } = useConfirmToken();
+  // const [message, setMessage] = useState({
+  //   header: '',
+  //   description: ''
+  // });
 
+  useEffect(() => {
+    console.log('Confirming token');
+    confirmToken(token);
 
-  return (
-    <div>
-      <h1>Confirmed!</h1>
-    </div>
-  );
+  }, [confirmToken, token]);
+
+  if (isSuccess) {
+    return (
+      <section className='confirmation_section'>
+        <h1>Successful! Your account is now confirmed and enabled!</h1>
+
+        <span className="material-icons-outlined icon green">
+          verified_user
+        </span>
+
+        <p>You can now login to QuizMe.</p>
+        <Link
+          to='/login'
+          title='Click here to login with your account!'
+        >
+          Click here to login!
+        </Link>
+      </section>
+    );
+  }
+  else if (isError) {
+    return (
+      <section className='confirmation_section'>
+        <h1>An unexpected error has occurred.</h1>
+
+        <span className="material-icons-outlined icon red">
+          error_outline
+        </span>
+
+        <p>{error.message}</p>
+        <Link
+          to='/home'
+          title='QuizMe | Home'
+        >
+          Click here to go home
+        </Link>
+      </section>
+    );
+  }
 }
